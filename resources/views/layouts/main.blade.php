ab45bf7c412f85a16360c128bed2512c29e1ef01
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-07-05 16:25:34 +0100

    Set up main template of  the application

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+@include('partials._head')
+<body>
+
+    @include("partials._nav")
+
+        <div class="container">
+            @include("partials._errors")
+
+            @yield('content')
+
+
+
+            @include("partials._footer")
+        </div> <!-- End of Container-->
+    @yield('script')
+</body>
+</html>
