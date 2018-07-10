ac9d8b90479918e50c3a329045d5ddbcce947b64
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-07-10 09:23:32 +0100

    Adding Customs Fields in Register Controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,13 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'phone' => 'required',
+            'address' => 'required',
+            'state' => 'required',
+            'city' => 'required',
+            'country' => 'required',
+            'postal_code' => 'required',
+            'logo' => 'required',
         ]);
     }
 
@@ -63,10 +70,20 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        //dd($data);
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+
+            //Customs Fields
+            'phone'          =>  $data['phone'],
+            'address'        =>  $data['address'],
+            'state'          =>   $data['state'],
+            'city'           =>    $data['city'],
+            'country'        =>    $data['country'],
+            'postal_code'    =>    $data['postal_code'],
+            'logo'           =>    $data['logo'],
         ]);
     }
 }
