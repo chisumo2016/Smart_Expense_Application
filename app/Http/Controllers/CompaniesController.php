f486bba38a51805853c5b3689c6a99e40aa2b1f8
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-07-11 09:22:45 +0100

    Companies Controller created successfully

---
+++ app/Http/Controllers/CompaniesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CompaniesController extends Controller
+{
+    //
+    public function index()
+    {
+
+    }
+
+    public  function create()
+    {
+
+    }
+
+    public  function  store()
+    {
+
+    }
+
+    public function active()
+    {
+
+    }
+}
