58eb6635a26d1fc66cee5b114318a9dcaf361896
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-07-11 09:18:09 +0100

    Expense Model created successfully

---
+++ app/Expense.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    //
+    protected  $table = 'expenses';
+    protected  $fillable =['user_id','company_id','budget_id','approver_id','priority','price','outside','subject',
+        'description','file','status','comments'];
+
+}
