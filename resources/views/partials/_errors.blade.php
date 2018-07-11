ccd70d909a9636882ed0ed522d18273267410e19
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-07-11 17:51:50 +0100

    Displaying Flash Messages - Company

---
+++ resources/views/partials/_errors.blade.php
@@ -0,0 +1,10 @@
+<div class="error-style">
+    @if(Session::has('message'))
+
+        <div class="alert alert-success">{{ Session::get('message') }}</div>
+    @endif
+
+    @if(Session::has('error'))
+        <div class="alert alert danger">{{ Session::get('error') }}</div>
+    @endif
+</div>
