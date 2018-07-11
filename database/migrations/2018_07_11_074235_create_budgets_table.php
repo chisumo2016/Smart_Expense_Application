8df55e37006100cc9164c38b0000310b37c8cf42
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-07-11 08:48:49 +0100

    Budgets migration created successfully

---
+++ database/migrations/2018_07_11_074235_create_budgets_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBudgetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('budgets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('company_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+            $table->integer('period_id')->unsigned();
+            $table->string('item');
+            $table->integer('unit');
+            $table->integer('quantity');
+            $table->integer('budget');
+            $table->timestamps();
+
+            //Foreign Key
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('period_id')->references('id')->on('periods')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('budgets');
+    }
+}
