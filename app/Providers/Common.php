04061ff6a0d40600bebd2d85b940dcb318668ce8
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-07-11 20:21:04 +0100

    Common File created in app->provider and Styling

---
+++ app/Providers/Common.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Providers;
+
+//use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class Common extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+
+
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    public static  function colors()
+    {
+        return [
+            'blue','yellow','red','magenta','green','violet','gray','brown','purple','orange'
+        ];
+    }
+}
